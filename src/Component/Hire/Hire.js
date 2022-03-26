5daf682b00313c3e72399fb009d09e38b260bfbf
owadud <akashahmed104466@gmail.com>
2022-03-26 15:15:07 +0600

    cart problem solved

--- src/Component/Hire/Hire.js
+++ src/Component/Hire/Hire.js
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import './Hire.css';
 const Hire = ({hire}) => {
+  console.log(hire);
+    
     return (
-        <div>
-             <h2>Hired Player</h2>
-                <p>selectedPlayer:{hire.length}</p>
+        <div className='hire-container'>
+             <h2>Want To Hire </h2>
+             {/* <p>length:{hire.length}</p> */}
+             <div className="hired-player">
+
+             <ul>
+                {
+                    hire.map(addHire =>
+                            
+                    
+                     <div className="listed-player">
+                    
+                    <img src={addHire.picture} alt="" />
+                    <p>Name: {addHire.name}</p>  
+                    <h3>Price: {addHire.price}</h3>
+                    
+                    </div>)
+
+                }
+            </ul>
+
+            </div>
+             
         </div>
     );
 };
